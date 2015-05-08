e2182f8bee616cb86184814fb27626150365dcef
acaria <eric.darchis@gmail.com>
2015-05-08 16:28:22 +0200

    add assets

---
+++ Textures/tileset/char1.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="char1" tilewidth="30" tileheight="21">
+ <tile id="0">
+  <properties>
+   <property name="profile" value="bat"/>
+  </properties>
+  <image width="30" height="21" source="char1/bat_icon.png"/>
+ </tile>
+</tileset>
---
+++ Textures/tileset/collision.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="collision" tilewidth="16" tileheight="16">
+ <tile id="0">
+  <properties>
+   <property name="collision" value="walkable"/>
+  </properties>
+  <image width="16" height="16" source="special/green.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collision" value="flyable"/>
+  </properties>
+  <image width="16" height="16" source="special/blue.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collision" value="crossing"/>
+  </properties>
+  <image width="16" height="16" source="special/purple.png"/>
+ </tile>
+</tileset>
